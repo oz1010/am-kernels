91c270c1ed20baf81f332c428680130fe462b9e7
oz1010 <uu90909@gmail.com>
2025-01-23 22:41:24 +0800

    feat: implement '%p' case

--- tests/am-tests/src/tests/hello.c
+++ tests/am-tests/src/tests/hello.c
@@ -1,7 +1,13 @@
 #include <amtest.h>
 
-void hello() {
-  for (int i = 0; i < 10; i ++) {
-    putstr("Hello, AM World @ " __ISA__ "\n");
-  }
+void hello()
+{
+    int *p = (int *)0x12569ae8;
+
+    printf("p:%p p:%p\n", p, p+1);
+
+    for (int i = 0; i < 10; i++)
+    {
+        putstr("Hello, AM World @ " __ISA__ "\n");
+    }
 }
