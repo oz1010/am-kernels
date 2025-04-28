e17352eff1755451b29f9c75a56928db9780ed26
oz1010 <uu90909@gmail.com>
2025-04-28 14:14:48 +0800

    feat: 修改非对齐读写测试用例

--- tests/cpu-tests/tests/unalign.c
+++ tests/cpu-tests/tests/unalign.c
@@ -27,5 +27,27 @@ int main() {
 	check(buf[5]==0xbb);
 	check(buf[6]==0xaa);
 
+	// /* 非对齐读取测试 */
+	for(int i = 0; i < 16; i++) buf[i] = i+1;
+	buf[1]=0xdd;
+	buf[2]=0xcc;
+	buf[3]=0xbb;
+	buf[4]=0xaa;
+	check(*((volatile unsigned*)(buf + 1)) == 0xaabbccdd);
+	
+	for(int i = 0; i < 16; i++) buf[i] = i+1;
+	buf[2]=0xdd;
+	buf[3]=0xcc;
+	buf[4]=0xbb;
+	buf[5]=0xaa;
+	check(*((volatile unsigned*)(buf + 2)) == 0xaabbccdd);
+	
+	for(int i = 0; i < 16; i++) buf[i] = i+1;
+	buf[3]=0xdd;
+	buf[4]=0xcc;
+	buf[5]=0xbb;
+	buf[6]=0xaa;
+	check(*((volatile unsigned*)(buf + 3)) == 0xaabbccdd);
+
 	return 0;
 }
