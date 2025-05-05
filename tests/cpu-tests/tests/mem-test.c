9f8c282168211347155d18b676d87148a2d3086d
oz1010 <uu90909@gmail.com>
2025-05-05 09:52:41 +0800

    feat: 新增psram内存测试用例

--- tests/cpu-tests/tests/mem-test.c
+++ tests/cpu-tests/tests/mem-test.c
@@ -1,12 +1,16 @@
-#include <stdint.h>
 #include "trap.h"
 #include "file/bin_header.h"
+#include "klib.h"
 
 extern bin_header_t bin_header;
 
 #define MEM_ADDR_START          0x0f000000 // 需要避免全局变量
 // #define MEM_ADDR_END            (_stack_pointer-4*12) // 注意不能覆盖栈区域，预留部分栈变量
 #define MEM_ADDR_END            (0x0f000400-4*12) // 注意不能覆盖栈区域，预留部分栈变量
+
+#define PSRAM_ENABLE            1
+#define PSRAM_ADDR_START        0x80000000
+#define PSRAM_ADDR_END          0x80001000
 
 int main() {
     // 简单测试，内存要求读写字节对齐
@@ -57,6 +61,46 @@ int main() {
             return test_cnt;
         }
     }
+
+#if(PSRAM_ENABLE)
+    printf("psram 8bit test ...");
+    pmem8 = (uint8_t *)PSRAM_ADDR_START;
+    for (uint32_t i=0; i<(PSRAM_ADDR_END-PSRAM_ADDR_START+1); ++i) {
+        uint8_t data = (uint8_t)((uint8_t)(uintptr_t)&pmem8[i]);
+        pmem8[i] = data;
+        if (pmem8[i] != data) {
+            printf(" failed, i=%d\n", i);
+            halt(i);
+        }
+    }
+    printf(" done\n");
+    
+    printf("psram 16bit test ...");
+    pmem16 = (uint16_t *)PSRAM_ADDR_START;
+    for (uint32_t i=0; i<(PSRAM_ADDR_END-PSRAM_ADDR_START+1); ++i) {
+        uint16_t data = (uint16_t)((uint16_t)(uintptr_t)&pmem16[i]);
+        pmem16[i] = data;
+        if (pmem16[i] != data) {
+            printf(" failed, i=%d\n", i);
+            halt(i);
+        }
+    }
+    printf(" done\n");
+    
+    printf("psram 32bit test ...");
+    pmem32 = (uint32_t *)PSRAM_ADDR_START;
+    for (uint32_t i=0; i<(PSRAM_ADDR_END-PSRAM_ADDR_START+1); ++i) {
+        uint32_t data = (uint32_t)((uint32_t)(uintptr_t)&pmem32[i]);
+        pmem32[i] = data;
+        if (pmem32[i] != data) {
+            printf(" failed, i=%d\n", i);
+            halt(i);
+        }
+    }
+    printf(" done\n");
+
+    halt(0);
+#endif
 
     return 0;
 }
